a8027df6dd154a6e0cb035efb265ad87ebc7819d
Ivan Lesar <ivan.lesar@degordian.com>
2020-03-29 20:02:18 +0200

    Setup tests

---
+++ tests/functional/GroceryItemControllerCest.php
@@ -0,0 +1,14 @@
+<?php namespace App\Tests;
+use Codeception\Util\HttpCode;
+
+class GroceryItemControllerCest
+{
+    // tests
+    public function tryToGetGroceryItems(FunctionalTester $I)
+    {
+//        $I->amLoggedAsAdmin();
+
+        $I->sendGET('/grocery/items');
+        $I->seeResponseCodeIs(HttpCode::OK);
+    }
+}
